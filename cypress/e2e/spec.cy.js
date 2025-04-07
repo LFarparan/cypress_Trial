a3bb978615c691ed0b66377f6939e6a287a45673
Lfarparan <aguinaldo.farparan@gmail.com>
2025-04-07 16:20:30 +0800

    Created automation test for login

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,22 @@
+describe('Log In', () => {
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/'); // Runs before every test
+  });
+
+  it('Verify login with valid credentials', () => {
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.contains('Swag Labs').should('be.visible')
+    cy.url().should("include", '/inventory.html')
+  })
+
+  it('Verify login with invalid login credentials', () => {
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('ehetenandayo')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]')
+    cy.url().should("include", 'saucedemo.com')
+  })
+
+})
