9a025f5dfc7db9d48bbe42624b3070fb08d52f08
Lfarparan <aguinaldo.farparan@gmail.com>
2025-04-08 10:24:17 +0800

    Test: Setup CI/CD pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
+// npx cypress run --record --key 51e42f93-e470-4b70-a2be-25c86e017030
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "a9wq9v",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
